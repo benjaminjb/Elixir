1e24481ee3574eed81488b1a794d0f9fd73a64ad
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-12-17 11:25:03 -0600

    Elixir Challenges -- O'Reilly Etudes: list minimum, maximum, range re-factor with outlier function

--- etudeslist.ex
+++ etudeslist.ex
@@ -4,28 +4,28 @@ defmodule EtudesList do
   http://chimera.labs.oreilly.com/books/1234000001642/ch06.html
   """
 
+  def outlier([], _func), do: []
+  def outlier([head | tail], func), do: _outlier(tail, func, head)
+
+  defp _outlier([], _func, current), do: current
+  defp _outlier([head | tail], func, current) do
+    if func.(head, current) do
+      _outlier(tail, func, head)
+    else
+      _outlier(tail, func, current)
+    end
+  end
+
   @doc "Find the minimum in a list of numbers."
   @spec minimum([number]) :: number
-  def minimum([]), do: []
-  def minimum([head | tail]), do: _minimum(tail, head)
+  def minimum(list), do: outlier(list, fn(n, m) -> n < m end)
   
-  # Private functions to find minimum
-  defp _minimum([], min), do: min
-  defp _minimum([head | tail], min) when head < min, do: _minimum(tail, head)
-  defp _minimum([_head | tail], min), do: _minimum(tail, min)
-
   @doc "Find the maximum in a list of numbers."
   @spec maximum([number]) :: number
-  def maximum([]), do: []
-  def maximum([head | tail]), do: _maximum(tail, head)
+  def maximum(list), do: outlier(list, fn(n, m) -> n > m end)
   
-  # Private functions to find maximum
-  defp _maximum([], max), do: max
-  defp _maximum([head | tail], max) when head > max, do: _maximum(tail, head)
-  defp _maximum([_head | tail], max), do: _maximum(tail, max)
-
   @doc "Find the minimum and maximum in a list of numbers."
   @spec range_find([number]) :: {number, number}
-  def range_find([]), do: []
-  def range_find([head | tail]), do: {_minimum(tail, head), _maximum(tail, head)} 
+  def range_find(list), do: {minimum(list), maximum(list)}
+
 end
