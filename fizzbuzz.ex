1df3a02419368e8aa5caf41933399fd29457ad82
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-12-17 10:36:09 -0600

    Fizzbuzz with multi-clause rather than case control flow

--- fizzbuzz.ex
+++ fizzbuzz.ex
@@ -1,17 +1,11 @@
 defmodule Challenges do
-
   def fizzbuzz(n) do
     1..n 
     |> Enum.map(&fbresult/1)
   end
 
-  defp fbresult(n) do
-    case {rem(n, 3), rem(n, 5)} do
-      {0,0} -> "fizzbuzz"
-      {0,_} -> "fizz"
-      {_,0} -> "buzz"
-      {_,_} -> n
-    end
-  end
-
+  defp fbresult(n) when rem(n, 3) == 0 and rem(n, 5) == 0, do: "FizzBuzz" 
+  defp fbresult(n) when rem(n, 3) == 0, do: "Fizz"
+  defp fbresult(n) when rem(n, 5) == 0, do: "Buzz"
+  defp fbresult(n), do: n
 end
